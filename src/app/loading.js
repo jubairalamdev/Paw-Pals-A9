c2c6a5e0f2cb8411c37bfd844d158bbab3ea4051
Jubair Alam <jubairalam.dev@gmail.com>
2026-06-17 00:11:21 +0600

    loading classnames fixed

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,19 +1,19 @@
 const GlobalLoading = () => {
     return (
         <main className="min-h-[85vh] flex flex-col justify-center items-center">
-            <div class="loader3">
-                <div class="bars bar1"></div>
-                <div class="bars bar2"></div>
-                <div class="bars bar3"></div>
-                <div class="bars bar4"></div>
-                <div class="bars bar5"></div>
-                <div class="bars bar6"></div>
-                <div class="bars bar7"></div>
-                <div class="bars bar8"></div>
-                <div class="bars bar9"></div>
-                <div class="bars bar10"></div>
+            <div className="loader3">
+                <div className="bars bar1"></div>
+                <div className="bars bar2"></div>
+                <div className="bars bar3"></div>
+                <div className="bars bar4"></div>
+                <div className="bars bar5"></div>
+                <div className="bars bar6"></div>
+                <div className="bars bar7"></div>
+                <div className="bars bar8"></div>
+                <div className="bars bar9"></div>
+                <div className="bars bar10"></div>
             </div>
-            <h3 className="text-[#4a6dd8] text-2xl pt-5 font-[family-name:var(--font-lilita)]">Loading...</h3>
+            <h3 className="text-[#4a6dd8] text-2xl pt-5 font-(family-name:--font-lilita)">Loading...</h3>
         </main>
     );
 };
