2b15fce9faede2974681da73a238e4fda3537f3a
Jubair Alam <jubairalam.dev@gmail.com>
2026-06-19 15:37:31 +0600

    logout toast showed

--- src/utils/authentication/logoutUser.js
+++ src/utils/authentication/logoutUser.js
@@ -1,5 +1,9 @@
 import { authClient } from "@/lib/auth-client";
+import { redirect } from "next/navigation";
+import { toast } from "react-toastify";
 
 export const logoutUser = async() =>{
     await authClient.signOut();
+    toast.success("Logged out Successfully")
+    redirect("/")
 }
