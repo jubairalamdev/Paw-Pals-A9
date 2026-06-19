b1a856d39836a4e1f5ef8ee0dc96031e427be8df
Jubair Alam <jubairalam.dev@gmail.com>
2026-06-19 15:31:47 +0600

    Layout for the dashboard created

---
+++ src/app/dashboard/layout.js
@@ -0,0 +1,17 @@
+import Sidebar from "@/Components/Dashboard/Sidebar";
+
+const DashboardLayout = ({children}) => {
+    return (
+        <div className="min-h-screen max-w-300 mx-auto grid grid-cols-12 gap-5">
+            <div className="md:col-span-3 col-span-12 pt-15 md:pt-0">
+
+            <Sidebar />
+            </div>
+            <main className="md:col-span-9 col-span-12 -mt-30 md:mt-0">
+                {children}
+            </main>
+        </div>
+    );
+};
+
+export default DashboardLayout;
