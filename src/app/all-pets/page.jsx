7a84cccbd378c2bbabc48cb8c596663c23ddc497
Jubair Alam <jubairalam.dev@gmail.com>
2026-06-19 15:31:34 +0600

    All pets section added in the page

--- src/app/all-pets/page.jsx
+++ src/app/all-pets/page.jsx
@@ -1,10 +1,12 @@
 import AllPets from "@/Components/AllPets/AllPets";
 import Testimonials from "@/Components/homepage/Testimonials";
+import { getAllPets } from "@/utils/actions";
 
-const AllPetsPage = () => {
+const AllPetsPage = async() => {
+      const pets = await getAllPets()
     return (
         <>
-            <AllPets />
+            <AllPets pets={pets}/>
             <Testimonials />
         </>
     );
